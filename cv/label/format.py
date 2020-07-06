4cf0cfaa49a315ab741aae798c8a49efffb3f48c
Samuel <sleeinsuk@gmail.com>
2020-07-06 04:08:39 -0700

    First commit

---
+++ cv/label/format.py
@@ -0,0 +1,31 @@
+from PIL import Image
+
+if __name__ == '__main__':
+   file_list = [line.rstrip('\n') for line in open("labels.list")]
+   num_file = len(file_list)
+   for i in range(num_file):
+       im = Image.open("Images/001/" + file_list[i][:-4] + ".JPEG")
+       width, height = im.size
+       x_scale = 1./width
+       y_scale = 1./height
+       label_file = open("YoloLabels/" + file_list[i], "a")
+       lines = [line.rstrip('\n') for line in open("Labels/001/" + file_list[i])]
+       print(lines)
+       print("foobar")
+       num_lines = len(lines)
+       for l in range(num_lines):
+           if l == 0:
+               continue
+
+           line = lines[l].split()
+           print(l, line)
+           x_center = float(line[0]) * x_scale
+           y_center = float(line[1]) * y_scale
+           x_width = float(line[2]) * x_scale
+           y_width = float(line[3]) * y_scale
+           if l == num_lines - 1:
+               label_file.write("0 " + str(x_center) + " " + str(y_center) + " " + str(x_width) + " " + str(y_width))
+               continue
+           label_file.write("0 " + str(x_center) + " " + str(y_center) + " " + str(x_width) + " " + str(y_width) + "\n")
+           print("0 " + str(x_center) + " " + str(y_center) + " " + str(x_width) + " " + str(y_width) + "\n")
+       print("Formatted " + file_list[i])
---
+++ cv/scrap/README.md
@@ -0,0 +1,3 @@
+Influenced by
+
+https://towardsdatascience.com/tutorial-build-an-object-detection-system-using-yolo-9a930513643a
